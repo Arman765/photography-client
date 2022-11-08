131e9c47d47b141dac5109374e29cace868db404
Arman765 <friend.of.laden765@gmail.com>
2022-11-09 02:24:12 +0600

    services

--- src/Pages/Home/Services/ServiceItem.js
+++ src/Pages/Home/Services/ServiceItem.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-const ServiceItem = () => {
-  return <div></div>;
+const ServiceItem = ({ service }) => {
+  const { _id, service_id, title, img, price, description } = service;
+  console.log(service);
+  return (
+    <div className="card card-compact w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img src={img} alt="" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{title}</h2>
+        <p>{description.slice(0, 99)}...</p>
+        <p>Price : ${price}</p>
+        <div className="card-actions justify-start">
+          <button className="btn btn-primary">Details</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ServiceItem;
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ServiceItem from "./ServiceItem";
 
 const Services = () => {
-  return <div></div>;
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/services")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+
+  return (
+    <div className="mb-10">
+      <div className="text-center mb-10 mt-10">
+        <p className="text-2xl font-bold"> Services</p>
+        <p className="text-2xl font-bold">
+          These are the services i give.As a professional i do my work very
+          sicerely and professionally
+        </p>
+      </div>
+      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ml-6">
+        {services.map((service) => (
+          <ServiceItem key={service._id} service={service}></ServiceItem>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Services;
