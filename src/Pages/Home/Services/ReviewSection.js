5f18dd5fae45e4a6b27de6d6d5207e37945979e4
Arman765 <friend.of.laden765@gmail.com>
2022-11-10 00:14:42 +0600

    review

--- src/Pages/Home/Services/ReviewSection.js
+++ src/Pages/Home/Services/ReviewSection.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../../../Contexts/AuthContext/AuthProvider";
 
 const ReviewSection = () => {
-  return <div></div>;
+  const { user } = useContext(AuthContext);
+  console.log(user);
+
+  return (
+    <div>
+      {user?.email ? (
+        <form>
+          <div className="flex ">
+            <div>
+              <img className="w-1/2" src={user.photoURL} alt=""></img>
+            </div>
+            <div>
+              <p className="text-xl ml-0">{user.displayName}</p>
+              <textarea
+                name="message"
+                className="textarea textarea-bordered h-10 w-full"
+              ></textarea>
+              <input className="btn" type="submit" value="Comment"></input>
+            </div>
+          </div>
+        </form>
+      ) : (
+        <div>
+          <p className="text-2xl font-semibold">
+            Please Login to place a review!
+          </p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ReviewSection;
